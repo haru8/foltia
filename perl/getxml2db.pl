3b7b7269079b941eb34c606f89485e488ca973ce
haru <haru.t84@gmail.com>
2015-11-12 14:30:26 +0900

    インデントとか整形。

--- perl/getxml2db.pl
+++ perl/getxml2db.pl
@@ -28,6 +28,7 @@ if ($path ne "./") {
 }
 require "foltialib.pl";
 
+&writelog("getxml2db.pl START.");
 
 $processes =  &processfind("getxml2db.pl");
 if ($processes > 1 ) {
@@ -72,135 +73,135 @@ close(SHOBO);
 #}
 #my (@line) = split(/\n/, $content);
 
-foreach(@line){
-s/\xef\xbd\x9e/\xe3\x80\x9c/g; #wavedash
-s/\xef\xbc\x8d/\xe2\x88\x92/g; #hyphenminus
-s/&#([0-9A-Fa-f]{2,6});/(chr($1))/eg; #'遊戯王5D&#039;s'とかの数値参照対応を
-
-Jcode::convert(\$_, 'utf8'); 
-
-#<ProgItem PID="21543" TID="528" StTime="20041114213000" EdTime="20041114220000" ChName="AT-X" Count="4" StOffset="0" SubTitle="いやだよ、サヨナラ…" Title="おとぎストーリー 天使のしっぽ" ProgComment=""/>
-if (/^<ProgItem /) { 
-s/<ProgItem //i;
-s/\"\/>/\" /i;
-s/\"[\s]/\";\n/gio;
-s/\'/\\'/gio;
-s/\"/\'/gio;
-#s/[\w]*=/\$item{$&}=/gio;
-#s/\=}=/}=/gio;
-s/(\w+)=/\$item{$1}=/gio;#by foltiaBBS
-
-#$item{PID}='21543';
-#$item{TID}='528';
-#$item{StTime}='20041114213000';
-#$item{EdTime}='20041114220000';
-#$item{ChName}='AT-X';
-#$item{Count}='4';
-#$item{StOffset}='0';
-#$item{SubTitle}='いやだよ、サヨナラ…';
-#$item{Title}='おとぎストーリー 天使のしっぽ';
-#$item{ProgComment}='';
-eval("$_");
-#Jcode::convert(\$item{Title},'utf8');
-
-$programtitlename = $item{Title};
-$programtitlename =~ s/\&lt\;/</gi;
-$programtitlename =~ s/\&gt\;/>/gi;
-$programtitlename =~ s/\&amp\;/\&/gi;
-#	$programtitle = $dbh->quote($programtitlename);
-	$programtitle = $programtitlename;
-
-#Jcode::convert(\$item{ChName},'utf8');
-#Jcode::convert(\$item{SubTitle},'utf8');
-
-#$programSubTitle = $dbh->quote($item{SubTitle});
-$programSubTitle = $item{SubTitle};
-$programSubTitle =~ s/\&lt\;/</gi;
-$programSubTitle =~ s/\&gt\;/>/gi;
-$programSubTitle =~ s/\&amp\;/\&/gi;
-#	$programSubTitle = $dbh->quote($programSubTitle);
-
-$offsetmin = $item{StOffset}/60;
-$edtime = &syobocaldate2foltiadate($item{EdTime});
-$sttime = &syobocaldate2foltiadate($item{StTime});
-$length = &calclength($sttime,$edtime);
-$recstartdate = &calcoffsetdate($sttime ,$offsetmin );
-$recenddate = &calcoffsetdate($edtime ,$offsetmin );
-
-$stationid = &getstationid($item{ChName});
-#サブタイトル追加-------------------------------------------------
-#番組があるか確認
-	$sth = $dbh->prepare($stmt{'getxml2db.1'});
-	$sth->execute($item{TID});
- @titlecount= $sth->fetchrow_array;
- 
-if ($titlecount[0] == 0) {
-	#なければ追加
-
-	#200412012359
-	$nomalstarttime = substr($sttime,8,4);
-
-	$sth = $dbh->prepare($stmt{'getxml2db.2'});
-	$oserr = $sth->execute($item{TID}, $programtitle, '', $nomalstarttime, $length, '', '', 3, 1, '', '');
-	&writelog("getxml2db  ADD TV Progtam:$item{TID}:$programtitle");
-} else {
-	#2006/2/26 
-	#あったら、タイトル確認して
-	$sth = $dbh->prepare($stmt{'getxml2db.3'});
-	$sth->execute($item{TID});
-	@titlearray = $sth->fetchrow_array;
-	#更新などされてたらupdate
-	#print "$titlearray[0] / $programtitle\n";
-	if ($titlearray[0] ne "$programtitlename" ) {
-		$sth = $dbh->prepare($stmt{'getxml2db.4'});
-		$oserr = $sth->execute($programtitle, $item{TID});
-		&writelog("getxml2db  UPDATE TV Progtam:$item{TID}:$programtitle");
-	} #end if update
-} # end if TID
-
-
-#PIDがあるか確認
-	$sth = $dbh->prepare($stmt{'getxml2db.5'});
-	$sth->execute($item{'TID'}, $item{'PID'});
-	@subticount= $sth->fetchrow_array;
-	if ($subticount[0]  >= 1) {
-		#PIDあったら上書き更新
-		#ここでこんなエラー出てる
-		#	DBD::Pg::st execute failed: ERROR:  invalid input syntax for type bigint: "" at /home/foltia/perl/getxml2db.pl line 147.
-		#UPDATE  foltia_subtitle  SET stationid = '42',countno = '8',subtitle = '京都行きます' ,startdatetime = '200503010035'  ,enddatetime = '200503010050',startoffset  = '0' ,lengthmin = '15' WHERE tid = '550' AND pid =  '26000' 
-		if ($item{Count} == "") {
-			$sth = $dbh->prepare($stmt{'getxml2db.6'});
-			$oserr = $sth->execute($stationid, undef, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length, $item{'TID'}, $item{'PID'});
-			#printf("getxml2db.6: $stationid, undef, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length, $item{'TID'}, $item{'PID'}\n");
-		} else {
-			$sth = $dbh->prepare($stmt{'getxml2db.7'});
-			$oserr = $sth->execute($stationid, $item{'Count'}, $programSubTitle,  $recstartdate, $recenddate, $offsetmin, $length, $item{'TID'}, $item{'PID'});
-			#printf("getxml2db.7: $stationid, $item{'Count'}, $programSubTitle,  $recstartdate, $recenddate, $offsetmin, $length, $item{'TID'}, $item{'PID'}\n");
-		}
-	} else {
-		#なければ追加
+foreach(@line) {
+	s/\xef\xbd\x9e/\xe3\x80\x9c/g; #wavedash
+	s/\xef\xbc\x8d/\xe2\x88\x92/g; #hyphenminus
+	s/&#([0-9A-Fa-f]{2,6});/(chr($1))/eg; #'遊戯王5D&#039;s'とかの数値参照対応を
+	
+	Jcode::convert(\$_, 'utf8'); 
+	
+	#<ProgItem PID="21543" TID="528" StTime="20041114213000" EdTime="20041114220000" ChName="AT-X" Count="4" StOffset="0" SubTitle="いやだよ、サヨナラ…" Title="おとぎストーリー 天使のしっぽ" ProgComment=""/>
+	if (/^<ProgItem /) { 
+		s/<ProgItem //i;
+		s/\"\/>/\" /i;
+		s/\"[\s]/\";\n/gio;
+		s/\'/\\'/gio;
+		s/\"/\'/gio;
+		#s/[\w]*=/\$item{$&}=/gio;
+		s/(\w+)=/\$item{$1}=/gio;#by foltiaBBS
 		
-		#こっちに入る時刻はオフセットされた時刻!
-		#そのままキューに入る形で
-		if ($item{Count} eq "") {
-			$sth = $dbh->prepare($stmt{'getxml2db.8'});
-			$oserr = $sth->execute($item{'PID'}, $item{'TID'}, $stationid, undef, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length);
-			#printf("getxml2db.8: $item{'PID'}, $item{'TID'}, $stationid, undef, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length\n");
+		#$item{PID}='21543';
+		#$item{TID}='528';
+		#$item{StTime}='20041114213000';
+		#$item{EdTime}='20041114220000';
+		#$item{ChName}='AT-X';
+		#$item{Count}='4';
+		#$item{StOffset}='0';
+		#$item{SubTitle}='いやだよ、サヨナラ…';
+		#$item{Title}='おとぎストーリー 天使のしっぽ';
+		#$item{ProgComment}='';
+		eval("$_");
+		#Jcode::convert(\$item{Title},'utf8');
+		
+		$programtitlename = $item{Title};
+		$programtitlename =~ s/\&lt\;/</gi;
+		$programtitlename =~ s/\&gt\;/>/gi;
+		$programtitlename =~ s/\&amp\;/\&/gi;
+		#$programtitle = $dbh->quote($programtitlename);
+		$programtitle = $programtitlename;
+		
+		#Jcode::convert(\$item{ChName},'utf8');
+		#Jcode::convert(\$item{SubTitle},'utf8');
+		
+		#$programSubTitle = $dbh->quote($item{SubTitle});
+		$programSubTitle = $item{SubTitle};
+		$programSubTitle =~ s/\&lt\;/</gi;
+		$programSubTitle =~ s/\&gt\;/>/gi;
+		$programSubTitle =~ s/\&amp\;/\&/gi;
+		#$programSubTitle = $dbh->quote($programSubTitle);
+		
+		$offsetmin = $item{StOffset}/60;
+		$edtime = &syobocaldate2foltiadate($item{EdTime});
+		$sttime = &syobocaldate2foltiadate($item{StTime});
+		$length = &calclength($sttime,$edtime);
+		$recstartdate = &calcoffsetdate($sttime ,$offsetmin );
+		$recenddate = &calcoffsetdate($edtime ,$offsetmin );
+		
+		$stationid = &getstationid($item{ChName});
+		#サブタイトル追加-------------------------------------------------
+		#番組があるか確認
+		$sth = $dbh->prepare($stmt{'getxml2db.1'});
+		$sth->execute($item{TID});
+		@titlecount= $sth->fetchrow_array;
+		 
+		if ($titlecount[0] == 0) {
+			#なければ追加
+		
+			#200412012359
+			$nomalstarttime = substr($sttime,8,4);
+		
+			$sth = $dbh->prepare($stmt{'getxml2db.2'});
+			$oserr = $sth->execute($item{TID}, $programtitle, '', $nomalstarttime, $length, '', '', 3, 1, '', '');
+			&writelog("getxml2db  ADD TV Progtam:$item{TID}:$programtitle");
 		} else {
-			$sth = $dbh->prepare($stmt{'getxml2db.9'});
-			$oserr = $sth->execute($item{'PID'}, $item{'TID'}, $stationid, $item{'Count'}, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length);
-			#printf("getxml2db.9: $item{'PID'}, $item{'TID'}, $stationid, $item{'Count'}, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length\n");
+			#2006/2/26 
+			#あったら、タイトル確認して
+			$sth = $dbh->prepare($stmt{'getxml2db.3'});
+			$sth->execute($item{TID});
+			@titlearray = $sth->fetchrow_array;
+			#更新などされてたらupdate
+			#print "$titlearray[0] / $programtitle\n";
+			if ($titlearray[0] ne "$programtitlename" ) {
+				$sth = $dbh->prepare($stmt{'getxml2db.4'});
+				$oserr = $sth->execute($programtitle, $item{TID});
+				&writelog("getxml2db  UPDATE TV Progtam:$item{TID}:$programtitle");
+			} #end if update
+		} # end if TID
+		
+		
+		#PIDがあるか確認
+		$sth = $dbh->prepare($stmt{'getxml2db.5'});
+		$sth->execute($item{'TID'}, $item{'PID'});
+		@subticount= $sth->fetchrow_array;
+		if ($subticount[0]  >= 1) {
+			#PIDあったら上書き更新
+			#ここでこんなエラー出てる
+			#	DBD::Pg::st execute failed: ERROR:  invalid input syntax for type bigint: "" at /home/foltia/perl/getxml2db.pl line 147.
+			#UPDATE  foltia_subtitle  SET stationid = '42',countno = '8',subtitle = '京都行きます' ,startdatetime = '200503010035'  ,enddatetime = '200503010050',startoffset  = '0' ,lengthmin = '15' WHERE tid = '550' AND pid =  '26000' 
+			if ($item{Count} == "") {
+				$sth = $dbh->prepare($stmt{'getxml2db.6'});
+				$oserr = $sth->execute($stationid, undef, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length, $item{'TID'}, $item{'PID'});
+				#printf("getxml2db.6: $stationid, undef, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length, $item{'TID'}, $item{'PID'}\n");
+			} else {
+				$sth = $dbh->prepare($stmt{'getxml2db.7'});
+				$oserr = $sth->execute($stationid, $item{'Count'}, $programSubTitle,  $recstartdate, $recenddate, $offsetmin, $length, $item{'TID'}, $item{'PID'});
+				#printf("getxml2db.7: $stationid, $item{'Count'}, $programSubTitle,  $recstartdate, $recenddate, $offsetmin, $length, $item{'TID'}, $item{'PID'}\n");
+			}
+		} else {
+			#なければ追加
+			
+			#こっちに入る時刻はオフセットされた時刻!
+			#そのままキューに入る形で
+			if ($item{Count} eq "") {
+				$sth = $dbh->prepare($stmt{'getxml2db.8'});
+				$oserr = $sth->execute($item{'PID'}, $item{'TID'}, $stationid, undef, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length);
+				#printf("getxml2db.8: $item{'PID'}, $item{'TID'}, $stationid, undef, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length\n");
+			} else {
+				$sth = $dbh->prepare($stmt{'getxml2db.9'});
+				$oserr = $sth->execute($item{'PID'}, $item{'TID'}, $stationid, $item{'Count'}, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length);
+				#printf("getxml2db.9: $item{'PID'}, $item{'TID'}, $stationid, $item{'Count'}, $programSubTitle, $recstartdate, $recenddate, $offsetmin, $length\n");
+			}
 		}
-	}
-
-#print "$DBQuery\n\n\n";
-#debug 20050803
-#&writelog("getxml2db $DBQuery");
-
-
-}#if
-}#foreach
+		
+		#print "$DBQuery\n\n\n";
+		#debug 20050803
+		#&writelog("getxml2db $DBQuery");
+		
+	} #if
+} #foreach
 
 $oserr = $dbh->commit;
 ##	$dbh->disconnect();
-#
+
+&writelog("getxml2db.pl END.");
+
