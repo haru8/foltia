65b7b91deec49b14a1787dd0f7198e31b103e210
haru <haru.t84@gmail.com>
2016-01-12 21:59:30 +0900

    インデントとか修正

--- php/titlelist.php
+++ php/titlelist.php
@@ -19,17 +19,16 @@ titlelist.php
 include("./foltialib.php");
 $con = m_connect();
 
-
-if ($useenvironmentpolicy == 1){
-if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header("WWW-Authenticate: Basic realm=\"foltia\"");
-    header("HTTP/1.0 401 Unauthorized");
-	redirectlogin();
-    exit;
-} else {
-login($con,$_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW']);
-}
-}//end if login
+if ($useenvironmentpolicy == 1) {
+	if (!isset($_SERVER['PHP_AUTH_USER'])) {
+		header("WWW-Authenticate: Basic realm=\"foltia\"");
+		header("HTTP/1.0 401 Unauthorized");
+		redirectlogin();
+		exit;
+	} else {
+		login($con,$_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW']);
+	}
+} // end if login
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
@@ -44,42 +43,46 @@ login($con,$_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW']);
 <?php
 
 //////////////////////////////////////////////////////////
-//１ページの表示レコード数
+// 1ページの表示レコード数
 $lim = 1000;
-//クエリ取得
+
+// クエリ取得
 $p = getgetnumform(p);
-//ページ取得の計算
+
+// ページ取得の計算
 list($st,$p,$p2) = number_page($p,$lim);
+
 ///////////////////////////////////////////////////////////
 
-	$now = date("YmdHi");   
+$now = date("YmdHi");   
 
-	$query = "
-SELECT 
-foltia_program.tid,
-foltia_program .title 
-FROM  foltia_program 
-ORDER BY foltia_program.tid  DESC
-LIMIT $lim OFFSET $st
-	";
-//	$rs = m_query($con, $query, "DBクエリに失敗しました");
+$query = "
+	SELECT 
+	  foltia_program.tid,
+	  foltia_program .title 
+	FROM foltia_program 
+	ORDER BY foltia_program.tid DESC
+	LIMIT $lim OFFSET $st
+";
+
 $rs = sql_query($con, $query, "DBクエリに失敗しました");
 $rowdata = $rs->fetch();
-          if (! $rowdata) {
-                die_exit("番組データがありません<BR>");
-                }
+if (! $rowdata) {
+	die_exit("番組データがありません<BR>");
+}
 
-	$query2 = "
-SELECT COUNT(*) AS cnt FROM foltia_program 
-	";
+$query2 = "
+	SELECT COUNT(*) AS cnt FROM foltia_program 
+";
+
 $rs2 = sql_query($con, $query2, "DBクエリに失敗しました");
 $rowdata2 = $rs2->fetch();
-          if (! $rowdata2) {
-                die_exit("番組データがありません<BR>");
-          }
+if (! $rowdata2) {
+	die_exit("番組データがありません<BR>");
+}
+
 //行数取得
 $dtcnt =  $rowdata2[0];
-
 ?>
 
 <body BGCOLOR="#ffffff" TEXT="#494949" LINK="#0047ff" VLINK="#000000" ALINK="#c6edff" >
@@ -93,10 +96,10 @@ printhtmlpageheader();
 <p align="left">全番組リストを表示します。</p>
 
 <?php
-		/* フィールド数 */
+// フィールド数
 $maxcols = $rs->columnCount();
-	
-//Autopager 
+
+// Autopager 
 echo "<div id=contents class=autopagerize_page_element />";
 ?>
 
@@ -110,26 +113,27 @@ echo "<div id=contents class=autopagerize_page_element />";
 	</thead>
 	
 	<tbody>
-		<?php
-			/* テーブルのデータを出力 */
 
-  do {
-				echo("<tr>\n");
+<?php
+// テーブルのデータを出力
+do {
+	echo("<tr>\n");
+	
+	// TID
+	echo("<td><a href=\"reserveprogram.php?tid=" .
+	htmlspecialchars($rowdata[0])  . "\">" .
+	htmlspecialchars($rowdata[0]) . "</a></td>\n");
 
-				//TID
-					echo("<td><a href=\"reserveprogram.php?tid=" .
-					htmlspecialchars($rowdata[0])  . "\">" .
-					htmlspecialchars($rowdata[0]) . "</a></td>\n");
-				      //タイトル
-				        echo("<td><a href=\"http://cal.syoboi.jp/progedit.php?TID=" .
-					htmlspecialchars($rowdata[0])  . "\" target=\"_blank\">" .
-					htmlspecialchars($rowdata[1]) . "</a></td>\n");
-					print "<td><A HREF = \"showlibc.php?tid=".htmlspecialchars($rowdata[0])."\">mp4</A></td>\n";
+	// タイトル
+    echo("<td><a href=\"http://cal.syoboi.jp/progedit.php?TID=" .
+	htmlspecialchars($rowdata[0])  . "\" target=\"_blank\">" .
+	htmlspecialchars($rowdata[1]) . "</a></td>\n");
+	print "<td><A HREF = \"showlibc.php?tid=".htmlspecialchars($rowdata[0])."\">mp4</A></td>\n";
 
-				echo("</tr>\n");
-    } while ($rowdata = $rs->fetch());
+	echo("</tr>\n");
+} while ($rowdata = $rs->fetch());
 
-		?>
+?>
 
 	</tbody>
 </table>
@@ -137,10 +141,11 @@ echo "<div id=contents class=autopagerize_page_element />";
 <?php
 
 /////////////////////////////////////////////////////////
-//Autopageing処理とページのリンクを表示
+// Autopageing処理とページのリンクを表示
 page_display("",$p,$p2,$lim,$dtcnt,"");
 ////////////////////////////////////////////////////////
 
 ?>
 </body>
 </html>
+
